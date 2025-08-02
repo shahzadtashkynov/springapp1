f06e76b5ea1679629dddf7cbe02f786dd8c2d13a
Shahzad <shahzadtashkynov@gmail.com>
2025-08-02 13:29:02 +0500

    Urok 12. Annotations @Scope

--- src/main/java/tm/shtashkynov/springcourse/MusicPlayer.java
+++ src/main/java/tm/shtashkynov/springcourse/MusicPlayer.java
@@ -2,9 +2,11 @@ package tm.shtashkynov.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class MusicPlayer {
 
 	private Music music1; 
